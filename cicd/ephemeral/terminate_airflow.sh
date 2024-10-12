6202f922815cb68cd873469441082682af072954
Kishore Veleti <vavkkishore@Kishores-MacBook-Air.local>
2024-10-12 18:21:05 -0400

    Stable version of the airflow

--- cicd/ephemeral/terminate_airflow.sh
+++ cicd/ephemeral/terminate_airflow.sh
@@ -3,5 +3,8 @@
 # Terminate Docker Compose containers, remove volumes, and clean up networks
 docker-compose -f ./cicd/ephemeral/airflow/docker-compose.yaml down --volumes --remove-orphans
 
+# Remove the postgres-data folder and its contents
+rm -rf ./cicd/ephemeral/airflow/postgres-data || true
+
 # Print a message indicating that Airflow has been terminated and cleaned up
 echo "Apache Airflow has been terminated and containers have been removed!"
---
+++ student_onboard_batch_service/dags/main_dag.py
@@ -0,0 +1,71 @@
+# student_onboard_batch_service/dags/main_dag.py
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+import os
+
+# Default path to read student data
+# Get the user's home directory
+user_home = os.path.expanduser("~")
+# Define the default path to read student data from the datasets folder
+default_path = os.path.join(user_home, "student_onboard_batch_service", "datasets", "staging_datazone")
+
+
+# Define the function that will be called by the PythonOperator
+def read_student_data_from_staging_datazone(staging_path=None, staging_path_provided=None, **kwargs):
+    # Determine which path to use based on the staging_path_provided flag
+    if staging_path_provided and staging_path:
+        path_to_use = staging_path
+    else:
+        path_to_use = default_path
+
+    # Logic to read student data from the specified path
+    print(f"Reading student data from: {path_to_use}")
+
+
+def trigger_bp_dag(**kwargs):
+    # Logic to trigger the BP_DAG for each student
+    print("Triggering BP_DAG for each student...")
+
+
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 10, 12),
+    'retries': 1,
+}
+
+# Define the DAG
+with DAG('Main_DAG',
+         default_args=default_args,
+         description='Main DAG for onboarding students',
+         schedule_interval='@daily',  # Change the schedule as needed
+         catchup=False) as dag:
+    # Dummy task to start the workflow
+    start = DummyOperator(task_id='start')
+
+    # Task to read student data with parameters for the path and flag
+    read_student_data_from_staging_datazone = PythonOperator(
+        task_id='read_student_data_from_stg_datazone',
+        python_callable=read_student_data_from_staging_datazone,
+        op_kwargs={
+            'staging_path': '{{ dag_run.conf.get("staging_path", "") }}',
+            # Get the staging path from DAG input params, default to None
+            'staging_path_provided': '{{ dag_run.conf.get("staging_path_provided", "false") }}'
+            # Get the boolean flag, default to None
+        },
+        provide_context=True
+    )
+
+    # Task to trigger the BP_DAG
+    trigger_bp = PythonOperator(
+        task_id='trigger_bp_dag',
+        python_callable=trigger_bp_dag,
+        provide_context=True
+    )
+
+    # Set the task dependencies
+    start >> read_student_data_from_staging_datazone >> trigger_bp
